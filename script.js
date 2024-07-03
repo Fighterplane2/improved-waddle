2756e5c13f8d33842ce268a1adcefcbf67bd848f
Jaeden Waller <146506880+Fighterplane2@users.noreply.github.com>
2024-07-03 17:58:20 +0000

    removed improper formatting, created p5js stuff

---
+++ script.js
@@ -0,0 +1,12 @@
+window.alert("The webpage has been sucessfully loaded.");
+
+function setup(){
+    createCanvas(500, 300);
+    background(255, 100, 60);
+    circle(width/2, height/2, 25);
+    // rectangle(width/1.5, height/3, 5);
+}
+
+function draw(){
+
+}
